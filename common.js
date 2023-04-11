61e7f58eaaab206a6b4436c57001a3066e0c0d16
fraxbart <119609549+fraxbart@users.noreply.github.com>
2023-04-11 22:31:52 +0200

    Update common.js

--- common.js
+++ common.js
@@ -20,8 +20,8 @@ const fetchApi = async (url, method = "GET", headers = null, payload = null) => 
 
 const login = async (username, password) => {
     const credentials = {
-        username: "carlocalandra@gmail.com",
-        password: "Test"
+        username: "francesco.bartt@gmail.com",
+        password: "dione1990A!"
     }
     let response = await fetchApi("https://striveschool-api.herokuapp.com/api/account/login", "POST", { "Content-Type": "application/json"}, credentials)
     token = response.access_token;
